f260190651a4e200eca9daeadd58c5d73b19e456
varun singh <singhvarun1904@gmail.com>
2024-07-28 18:02:06 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import NavBar from './components/NavBar';
+import AvailableDataChart from './components/AvailableDataChart';
+import AssignedDataChart from './components/AssignedDataChart';
+import AnnotatedDataChart from './components/AnnotatedDataChart';
+import UnannotatedDataChart from './components/UnannotatedDataChart';
+import DataCard from './components/DataCard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Container fluid>
+        <Row>
+          <Col><DataCard title="Number of Annotators" value={23} /></Col>
+          <Col><DataCard title="Number of Linguistics" value={43} /></Col>
+          <Col><DataCard title="Number of Groups" value={43} /></Col>
+        </Row>
+        <Row>
+          <Col><AvailableDataChart /></Col>
+          <Col><AssignedDataChart /></Col>
+        </Row>
+        <Row>
+          <Col><AnnotatedDataChart /></Col>
+          <Col><UnannotatedDataChart /></Col>
+        </Row>
+      </Container>
     </div>
   );
 }
