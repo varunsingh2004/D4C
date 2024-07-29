18f0faf7f7259c40331bd661ab40fbbce3735465
AyushBalyan1301 <ayush.balyan13@gmail.com>
2024-07-29 10:05:53 +0530

    Minor Donut Chart Update

--- src/Components/DonutSentimentChart.js
+++ src/Components/DonutSentimentChart.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect } from 'react';
-import { Chart } from 'chart.js';
+import { Chart } from 'chart.js/auto';
 
 function DonutChart() {
   const chartRef = useRef(null);
